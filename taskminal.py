f9a8e4c10578c4374715fc73d30cf8337c64a448
JustADataConstruct <yarukatsaros@gmail.com>
2021-12-07 17:09:29 +0100

    Command scaffolding

--- taskminal.py
+++ taskminal.py
@@ -2,6 +2,8 @@ import os
 import sqlite3
 from sqlite3.dbapi2 import sqlite_version
 from sqlite3 import Error
+import argparse
+import sys
 
 def init_new_database(name:str = "taskminal.db",force:bool = False) -> bool:
     if os.path.isfile(name):
@@ -77,4 +79,33 @@ def close_connection(conn):
     return conn
 
 if __name__ == "__main__":
-    print("Hello world")
+    parser = argparse.ArgumentParser(prog='taskminal')
+    subparsers = parser.add_subparsers(title="Action",help="The action to run.",required=True,dest="subparser")
+
+    parser_create = subparsers.add_parser("createdb",help="Create a new database")
+    parser_create.add_argument("name",help="Name of the new database.")
+    parser_create.add_argument("-f",action="store_true",help="Force the creation of this database even if it already exists.")
+
+    parser_add_task = subparsers.add_parser("new",help="Adds a new task")
+    parser_add_task.add_argument("title",help="Name of the task")
+
+    parser_list = subparsers.add_parser("list",help="Lists all tasks")
+    parser_list.add_argument("-c",action="store_true",help="Show only completed tasks.")
+    parser_list.add_argument("-u",action="store_true",help="Show only unfinished tasks.")
+    
+    parser_get = subparsers.add_parser("get",help="Gets info for a single task by its index.")
+    parser_get.add_argument("index",help="Index of the task you're searching for.")
+
+    parser_delete = subparsers.add_parser("delete",help="Removes a task by its index.")
+    parser_delete.add_argument("index",help="Index of the task you want to remove.")
+
+    parser_update = subparsers.add_parser("update",help="Sets the current date/time as the last time you worked on this task, but keeps it open.")
+    parser_update.add_argument("index",help="Index of the task you want to update.")
+
+    parser_complete = subparsers.add_parser("done",help="Completes the task with the given index.")
+    parser_complete.add_argument("index",help="Index of the task.")
+
+
+
+    args = parser.parse_args()
+    print(args)
